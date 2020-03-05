4744f96412661de50f16abc52aa500b543bb2457
Arthur Knoepflin <arthur@knoepflin.eu>
2020-03-05 20:14:30 +0000

    [~] fix device name length minimum

--- src/list_devices.c
+++ src/list_devices.c
@@ -42,11 +42,15 @@ int list_devices() {
     char errbuf[PCAP_ERRBUF_SIZE];
     int device_length;
     pcap_if_t *devices;
+
     if ((err = pcap_findalldevs(&devices, errbuf)) != 0 || devices == NULL) {
         printf("Cannot list device:\ncode %d\nmessage: %s", err, errbuf);
         return (84);
     }
     device_length = max_device_name_length(devices);
+    if (device_length < 14) {
+        device_length = 14;
+    }
     printf("%*s %5s %12s %11s\n", device_length, "Device name", "Up", "Connected", "Wireless");
     while (devices) {
         print_device(devices, device_length);
